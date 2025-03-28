91bc9be4c886e0da5835b1e5bc2c3bd228ff7345
Sahil Jagdale <sahiljagdale1073@gmail.com>
2025-03-28 17:34:28 +0530

    add user and video model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose, {Schema} from "mongoose";
-
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 const userScheme = new mongoose.Schema(
     {
         username: {
@@ -46,4 +47,39 @@ const userScheme = new mongoose.Schema(
     }
     ,{timestamps: true})
 
-export const User = mongoose.model("User",userScheme);
+    userScheme.pre("save", async function (next) {
+        if(!this.isModified("password")) return next();
+        this.password = bcrypt.hash(this.password, 10)
+        next();
+    })
+
+    userScheme.methods.isPasswordCorrect = async function (password) {
+        return await bcrypt.compare(password, this.password);
+    }
+    userScheme.methods.generateAccessToken = function () {
+        return jwt.sign(
+            {
+                _id: this._id,
+                email: this.email,
+                username: this.username,
+                fullName: this.fullName
+            },
+            process.env.ACCESS_TOKEN_SECRET,
+            {
+                expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+            }
+        )
+    }
+    userScheme.methods.generateRefreshToken = function () {
+        return jwt.sign(
+            {
+                _id: this._id
+            },
+            process.env.REFRESH_TOKEN_SECRET,
+            {
+                expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+            }
+        )
+    }
+
+    export const User = mongoose.model("User",userScheme);
